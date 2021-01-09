29860f5b7bedb5f76df343cdd92ad83e26dac5a9
Saroj Adhikari <adh.saroj@gmail.com>
2021-01-09 09:47:22 -0500

    change dataset_definition variable name to dataset inside the class to avoid name confusion

--- moodle_questions/questions/calculated.py
+++ moodle_questions/questions/calculated.py
@@ -43,7 +43,7 @@ class CalculatedQuestion(Question):
                                          self.minimums[i],
                                          self.maximums[i],
                                          self.decimals[i]
-                                         )
+                                         )._to_xml_element()
             dataset_definitions.append(dataset)
         
         return question
@@ -61,7 +61,7 @@ class CalculatedQuestion(Question):
 class dataset_definition:
     """
     This class defines the name, type, distribution etc of the
-    dataset along with max, min, ndata etc.
+    dataset along with max, min, itemcount etc.
 
     There is also a method to generate itemcount dataset_items
     for each variable specified.
@@ -79,37 +79,37 @@ class dataset_definition:
         self.decimals = decimals
 
     def _to_xml_element(self, st="private"):
-        dataset_definition = et.Element("dataset_definition")
-        status = et.SubElement(dataset_definition, "status")
+        dataset = et.Element("dataset_definition")
+        status = et.SubElement(dataset, "status")
         text = et.SubElement(status, "text")
         text.text = st   # shared or private 
 
-        name = et.SubElement(dataset_definition, "name")
+        name = et.SubElement(dataset, "name")
         text = et.SubElement(name, "text")
         text.text = self.variable
 
         #type=calculated?
 
-        distribution = et.SubElement(dataset_definition, "distribution")
+        distribution = et.SubElement(dataset, "distribution")
         text = et.SubElement(distribution, "text")
         text.text = self.distribution
 
-        minimum = et.SubElement(dataset_definition, "minimum")
+        minimum = et.SubElement(dataset, "minimum")
         text = et.SubElement(minimum, "text")
         text.text = str(self.minimum)
 
-        maximum = et.SubElement(dataset_definition, "maximum")
+        maximum = et.SubElement(dataset, "maximum")
         text = et.SubElement(maximum, "text")
         text.text = str(self.maximum)
 
-        decimals = et.SubElement(dataset_definition, "decimals")
+        decimals = et.SubElement(dataset, "decimals")
         text = et.SubElement(decimals, "text")
         text.text = str(self.decimals)
 
-        itemcount = et.SubElement(dataset_definition, "itemcount")
+        itemcount = et.SubElement(dataset, "itemcount")
         itemcount.text = str(self.itemcount)
 
-        dataset_items = et.SubElement(dataset_definition, "dataset_items")
+        dataset_items = et.SubElement(dataset, "dataset_items")
 
         for i in range(1, self.itemcount+1):
             dataset_item = et.SubElement(dataset_items, "dataset_item")
@@ -119,10 +119,10 @@ class dataset_definition:
             value = et.SubElement(dataset_item, "value")
             value.text = str(round(uniform(self.minimum, self.maximum), self.decimals))
 
-        number_of_items = et.SubElement(dataset_definition, "number_of_items")
+        number_of_items = et.SubElement(dataset, "number_of_items")
         number_of_items.text = str(self.itemcount)
 
-        return dataset_definition            
+        return dataset            
 
 
 
